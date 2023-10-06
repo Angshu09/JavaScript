2c7e63590fbfa4356df768eee30cf2874087733f
Angshu Das <136366365+Angshu09@users.noreply.github.com>
2023-10-06 19:15:03 +0000

    Datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as its newer version
+
+//alert(3+3) //we are using node js not browser
+//documentation of JS - "tc39" and "mdn"
+
+let name = "Angshu"
+let age = 18
+let isIt = true
+
+//number -> 2 to power 53
+//bigint
+//string
+//boolean
+//null -> stand alone value
+//undefined
+//symbol -> unique
+
+//object
+
+console.log(typeof null); //null itself is an object
+console.log(typeof undefined); //undefined type 
