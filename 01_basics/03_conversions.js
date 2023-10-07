bb348708c3f2db193b4cbc58a750437b11ac3340
Angshu Das <136366365+Angshu09@users.noreply.github.com>
2023-10-07 18:59:17 +0000

    Conversion on datatypes

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,29 @@
+// let score = 67
+// console.log(typeof score);
+
+//  let actual = String(score)
+// console.log(typeof actual)
+// console.log(actual) // This is string 
+
+/*let b = "33"
+let c = "67uhg"
+let d = true*/
+
+
+// console.log(typeof c);
+// let realval = Number(c)
+// console.log(typeof realval);
+// console.log(realval); // It throws the value called NaN (Not a number) because alphabets are present with the number
+
+
+// let realval1 = Number(d)
+// console.log(realval1); // It throws me 1 
+
+
+// "33" => 33
+// "67uhg" => NaN
+// true => 1, false => 0
+// 1 => true, 0 => false  
+// " " => false
+// "Angshu" => true              
+
