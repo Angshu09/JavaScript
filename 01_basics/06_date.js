3288f42c9929ec43ee1a6610f3cae9fd79be0817
Angshu Das <136366365+Angshu09@users.noreply.github.com>
2023-10-11 18:23:46 +0000

    Date in JS

---
+++ 01_basics/06_date.js
@@ -0,0 +1,17 @@
+//Date a object in javaScript
+
+let myDate = new Date();
+console.log(myDate); //2023-10-11T18:11:37.006Z
+console.log(myDate.toString()); //Wed Oct 11 2023 18:11:37 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toISOString()); //2023-10-11T18:12:50.598Z
+console.log(myDate.toJSON()); //2023-10-11T18:12:50.598Z
+console.log(myDate.toLocaleString()); //10/11/2023, 6:16:01 PM
+
+let anotherMyDate = new Date(2023, 0, 24);
+console.log(anotherMyDate.toDateString()); //Tue Jan 24 2023
+
+let anotherMyDate2 = new Date(2023, 1, 23);
+console.log(anotherMyDate2.toDateString()); //Thu Feb 23 2023
+
+let myCreateDate = new Date(2023, 0, 25, 6, 4);
+console.log(myCreateDate.toLocaleString()); //1/25/2023, 6:04:00 AM
