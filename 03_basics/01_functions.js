c84bc43bf0ce2d94e8871955dc8dbb8904970cc6
Angshu Das <angshudas191@gmail.com>
2024-01-17 00:44:27 +0530

    added push and arrow functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,4 +8,34 @@ function greeting(name = "mr-x"){
     return `${name} Hi! Good morning`;
 }
 
-console.log(greeting("Angshu"));
+// console.log(greeting("Angshu"));
+
+function show(...number){
+    return number;
+}
+
+// console.log(show(45, 67, 89, 12)); // [ 45, 67, 89, 12 ]
+
+const person = {
+    name: "Angshu",
+    year: 2024
+}
+
+function showObj(anyObjcet){
+    console.log(`hey ${anyObjcet.name} this is your 2nd year ${anyObjcet.year}`);
+}
+
+// showObj(person) //hey Angshu this is your 2nd year 2024
+
+showObj({
+    name: "mr-x",
+    year: 2000
+})  // hey mr-x this is your 2nd year 2000
+
+function secondIndex(anyArray){
+    return anyArray[1];
+}
+
+const arr = [3, 6, 8, 9, 1];
+console.log(secondIndex(arr)); //6
+console.log(secondIndex([5, 8, 12, 5])); //8
---
+++ 03_basics/02_arrowFunction.js
@@ -0,0 +1,10 @@
+const peron = {
+    name: "Angshu",
+    college: "MAKAUT",
+
+    greeting: function(){
+        console.log(`${this.name} , is a student of ${this.college}`);
+    }
+}
+
+peron.greeting();
