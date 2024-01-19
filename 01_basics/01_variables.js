d87a3aefd2dd902181aa94109a1c93c045f89762
Angshu Das <angshudas191@gmail.com>
2024-01-19 22:07:01 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 7890
 var accountEmail = "Angshu.Google.com"
 let accountPassword = "1234"
-accountCity = "Patna"
+let accountCity = "Patna"
 let accountState
 
 //accountId = 8907 ---> this is not allowed because the const value is constant we can't change it
